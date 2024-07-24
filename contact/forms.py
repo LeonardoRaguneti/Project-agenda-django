3f6e713c87e66c814ea37ccf5685a9adfa5f5c2f
LeonardoRaguneti <leozz993@outlook.com>
2024-07-24 04:24:52 -0300

    update/delete users

--- contact/forms.py
+++ contact/forms.py
@@ -4,19 +4,50 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(                     # se isto form comentado ele usara o que esta no models.py
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone')
-    
-    def clean(self):
+        fields = ('first_name', 'last_name', 'phone',
+                  'email', 'description', 'category')
+
+
+    def clean(self):                                       # ideal para validação de mais de um valor.
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Segundo nome não pode ser igual ao primeiro',
+                code='invalid'
+            )
+            self.add_error('last_name', msg)
         
-        self.add_error(
-            'first_name', 
-            ValidationError(
-                'Mensagem de erro',
+        return super().clean()
+    
+
+    def clean_first_name(self):                             # ideal para validação de apenas um valor.
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error('first_name', ValidationError(
+                'Não digite "ABC" neste campo.', 
                 code='invalid'
             )
         )
 
-        return super().clean()
+        return first_name
--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
 
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
